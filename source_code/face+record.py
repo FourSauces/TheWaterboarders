d0fc8a4ee3de47d6a18300f7bdb8715cc5e8e926
Gymnast544 <oberoiarjun2009@gmail.com>
2023-02-18 18:53:29 -0800

    updated face record

--- source_code/face+record.py
+++ source_code/face+record.py
@@ -4,53 +4,57 @@ import face_recognition
 import threading
 import time
 
-my_path = "/Users/judyliu/Documents/treehacks2023/TheWaterboarders/opencv-webcam"
-os.chdir(my_path + "/source_code/")
+os.chdir("source_code")
 x_c = 0 
 y_c = 0
 class WebcamRecorder:
-    def __init__(self, filename,duration):
+    def __init__(self, filename):
         self.filename = filename
-        self.duration = duration
+        self.frame = None
+        self.centerFace = (-1, -1)
+        self.isRecording = False
         self.frames = []
+        self.prevFaces = []
         self.fps = 60
         self.stop_event = threading.Event()
+        self.cap = None
 
     def start(self):
-        cap = cv2.VideoCapture(0)
-        cap.set(cv2.CAP_PROP_FPS, self.fps)  # Set to 30 frames per second
-        t = threading.Thread(target=self._record, args=(cap,))
+        self.cap = cv2.VideoCapture(0)
+        self.cap.set(cv2.CAP_PROP_FPS, self.fps)  # Set to 30 frames per second
+        t = threading.Thread(target=self.updateFrameLoop, args=(self))
         t.start()
 
-    def stop(self):
-        self.stop_event.set()
 
-    def _record(self, cap):
-        start_time = cv2.getTickCount()
-        while not self.stop_event.is_set():
-            ret, frame = cap.read()
-            if not ret:
-                break
-            rgb_frame = frame[:, :, ::-1]
-            face_locations = face_recognition.face_locations(rgb_frame)
-            if len(face_locations) == 0:
-                print("nofacedetected")
-                continue
-            middle_point = (frame.shape[1] // 2, frame.shape[0] // 2)
-            closest_face_location = min(face_locations, key=lambda loc: (loc[0]+loc[2] - middle_point[0])**2 + (loc[1]+loc[3] - middle_point[1])**2)
-            top, right, bottom, left = closest_face_location
-            the_center = ((left+right)//2, (top+bottom)//2)
-            global x_c
-            x_c = (left+right)//2
-            global y_c
-            y_c =  (top+bottom)//2
-            print(the_center)
-            # cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
-            self.frames.append(frame)
-        cap.release()
-        cv2.destroyAllWindows()
-        self._save()
+    def getCoords(self, frame):
+        rgb_frame = frame[:, :, ::-1]
+        face_locations = face_recognition.face_locations(rgb_frame)
+        if len(face_locations) == 0:
+            print("nofacedetected")
+            return (-1, -1)
+        middle_point = (frame.shape[1] // 2, frame.shape[0] // 2)
+        closest_face_location = min(face_locations, key=lambda loc: (loc[0]+loc[2] - middle_point[0])**2 + (loc[1]+loc[3] - middle_point[1])**2)
+        top, right, bottom, left = closest_face_location
+        the_center = ((left+right)//2, (top+bottom)//2)
+        return the_center
+    
+    def updateFrame(self):
+        ret, frame = self.cap.read()
+        if not ret:
+            self.frame = frame
+            if self.isRecording():
+                self.frames.append(frame)
+            self.centerFace = self.getCoords(frame)
+            print("Center of face", self.centerFace)
+            #diy fifo queue
+            self.prevFaces.append(self.centerFace)
+            if len(self.prevFaces>30):
+                self.prevFaces.pop(0)
 
+    def updateFrameLoop(self):
+        while True:
+            self.updateFrame()
+    
     def _save(self):
         if len(self.frames) == 0:
             return
@@ -61,10 +65,13 @@ class WebcamRecorder:
             out.write(frame)
             time.sleep(1/self.fps)  # wait between frames
         out.release()
+        self.frames = []
 
-
-recorder = WebcamRecorder("output.mp4", duration=2)
-recorder.start()
-input("Press enter to stop recording...")
-recorder.stop()
+if __name__ == "__main__":
+    recorder = WebcamRecorder("output.mp4")
+    recorder.start()
+    recorder.isRecording = True
+    time.sleep(5)
+    recorder.isRecording = False
+    recorder.save()
 
